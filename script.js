1917440329bbe671088350ebf7c1b737416dff1e
Nickey Sharma <126977002+NickeySharma@users.noreply.github.com>
2023-05-15 22:06:22 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,44 @@
+//Mobile Menu
+let nav = document.querySelector("nav");
+let body = document.querySelector("body");
+let navBar = document.querySelector(".navbar");
+let menuBtn = document.querySelector(".menu-btn");
+let cancelBtn = document.querySelector(".cancel-btn");
+let logoOne = document.querySelector(".logo");
+
+menuBtn.onclick = function(){
+    navBar.classList.add("active");
+    menuBtn.style.opacity ="0";
+    menuBtn.style.pointerEvents = "none";
+    body.style.overflow = "hidden";
+}
+cancelBtn.onclick = function(){
+    navBar.classList.remove("active");
+    menuBtn.style.opacity ="1";
+    menuBtn.style.pointerEvents = "auto";
+    body.style.overflow = "auto";
+}
+
+//slide nav bar close after click
+let navLinks = document.querySelectorAll(".menu li a");
+for(var i = 0; i< navLinks.length; i++){
+    navLinks[i].addEventListener("click", function(){
+     navBar.classList.remove("active");
+    menuBtn.style.opacity ="1";
+    menuBtn.style.pointerEvents = "auto";
+    body.style.overflow = "auto";
+    })
+}
+
+//Scroll Fixed
+window.onscroll = function(){
+    if(document.documentElement.scrollTop > 20){
+        nav.classList.add("sticky");
+        logoOne.classList.add("active");
+        menuBtn.classList.add("active");
+    }else{
+        nav.classList.remove("sticky");
+        logoOne.classList.remove("active");
+        menuBtn.classList.remove("active");
+    }
+}
